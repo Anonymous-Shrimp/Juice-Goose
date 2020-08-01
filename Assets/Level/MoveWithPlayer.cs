1c54a2d7ad0d14554bf34d48c0d71266dde2d6e8
Anonymous Shrimp <yumiswep@gmail.com>
2020-08-01 13:55:25 -0700

    Basic Scene

---
+++ Assets/Level/MoveWithPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float xOffset = 5;
+    public float zOffset = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + xOffset, 0, zOffset);
+    }
+}
